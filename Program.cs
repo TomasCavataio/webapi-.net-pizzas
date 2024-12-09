5cfaea51e28ca5988c250eac5f2c71b184488c93
Pedro Hurtado <pedrohurt@gmail.com>
2024-12-09 20:23:58 +0100

    ingredient add

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ using webapi.core.IFeaturModule;
 using webapi.core.ioc;
 using webapi.core.repository;
 using webapi.domain.pizza;
+using webapi.infraestructura;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +12,20 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddInjectables();
+
+//builder.Services.AddTransient
+
+//cada vez que tu solicitas la interface hace un new de la clase
+
+//builder.Services.AddScoped
+
+//cada vez que se crea un request se crea un new de la clase
+//y las interfaces están disponibles
+
+//builder.Services.AddSingleton
+
+//se crean las intancias de las clases una sola vez
+//y las interfaces están disponibles como singleton
 
 var app = builder.Build();
 
